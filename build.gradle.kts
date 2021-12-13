bd1f3a20b477a8ebe3569ae821155e8fc1ade650
TehBrian <tehbrian@protonmail.com>
2021-12-12 22:37:53 -0500

    build: target Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ group = "xyz.tehbrian"
 version = "0.1.0-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_16
-    targetCompatibility = JavaVersion.VERSION_16
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
