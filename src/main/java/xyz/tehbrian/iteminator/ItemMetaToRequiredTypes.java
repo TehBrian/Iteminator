fee8e4b8de0bf956dc42049468d7ab52c9e926b1
TehBrian <tehbrian@protonmail.com>
2021-12-13 12:00:29 -0500

    get required types from item meta

---
+++ src/main/java/xyz/tehbrian/iteminator/ItemMetaToRequiredTypes.java
@@ -0,0 +1,34 @@
+package xyz.tehbrian.iteminator;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.TropicalFishBucketMeta;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// This entire class can be incinerated once switch pattern-matching arrives.
+public class ItemMetaToRequiredTypes {
+
+    private static final Map<Class<? extends ItemMeta>, List<Material>> converter;
+
+    static {
+        converter = new HashMap<>();
+        converter.put(TropicalFishBucketMeta.class, List.of(Material.TROPICAL_FISH_BUCKET));
+    }
+
+    private ItemMetaToRequiredTypes() {
+    }
+
+    /**
+     * @param itemMetaType the {@code ItemMeta} type
+     * @return the types required for that {@code ItemMeta} or null if there is no type requirement
+     */
+    public static @Nullable List<Material> get(final @NonNull Class<? extends ItemMeta> itemMetaType) {
+        return converter.get(itemMetaType);
+    }
+
+}
