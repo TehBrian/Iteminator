d9e66757ec4dede80795f05ca10435e51a479e1f
TehBrian <tehbrian@protonmail.com>
2021-12-29 13:13:57 -0500

    add special permission prefix to potion and suspicious stew

--- src/main/java/xyz/tehbrian/iteminator/util/Permissions.java
+++ src/main/java/xyz/tehbrian/iteminator/util/Permissions.java
@@ -8,27 +8,27 @@ public final class Permissions {
     public static final String ROOT = "iteminator";
     public static final String RELOAD = ROOT + ".reload";
 
-    public static final String FORMAT = ROOT + ".format";
+    public static final String FORMAT =       ROOT + ".format";
     public static final String MINI_MESSAGE = FORMAT + ".mini-message";
-    public static final String LEGACY = FORMAT + ".legacy";
+    public static final String LEGACY =       FORMAT + ".legacy";
 
-    public static final String AMOUNT = ROOT + ".amount";
-    public static final String MATERIAL = ROOT + ".material";
-    public static final String NAME = ROOT + ".name";
+    public static final String AMOUNT =      ROOT + ".amount";
+    public static final String MATERIAL =    ROOT + ".material";
+    public static final String NAME =        ROOT + ".name";
     public static final String UNBREAKABLE = ROOT + ".unbreakable";
-    public static final String LORE = ROOT + ".lore";
+    public static final String LORE =        ROOT + ".lore";
     public static final String ENCHANTMENT = ROOT + ".enchantment";
-    public static final String FLAGS = ROOT + ".flags";
+    public static final String FLAGS =       ROOT + ".flags";
 
-    public static final String SPECIAL = ROOT + ".special";
-    public static final String ARMOR_STAND = SPECIAL + ".armor-stand";
-    public static final String AXOLOTL_BUCKET = SPECIAL + ".axolotl-bucket";
-    public static final String BANNER = SPECIAL + ".banner";
-    public static final String BOOK = SPECIAL + ".book";
-    public static final String ENCHANTMENT_STORAGE = SPECIAL + ".enchantment-storage";
-    public static final String LEATHER_ARMOR = ".leather-armor";
-    public static final String POTION = SPECIAL + ".potion";
-    public static final String SUSPICIOUS_STEW = ".suspicious-stew";
+    public static final String SPECIAL =              ROOT + ".special";
+    public static final String ARMOR_STAND =          SPECIAL + ".armor-stand";
+    public static final String AXOLOTL_BUCKET =       SPECIAL + ".axolotl-bucket";
+    public static final String BANNER =               SPECIAL + ".banner";
+    public static final String BOOK =                 SPECIAL + ".book";
+    public static final String ENCHANTMENT_STORAGE =  SPECIAL + ".enchantment-storage";
+    public static final String LEATHER_ARMOR =        SPECIAL + ".leather-armor";
+    public static final String POTION =               SPECIAL + ".potion";
+    public static final String SUSPICIOUS_STEW =      SPECIAL + ".suspicious-stew";
     public static final String TROPICAL_FISH_BUCKET = SPECIAL + ".tropical-fish-bucket";
 
     private Permissions() {
