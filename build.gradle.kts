6f8a42fc620af799540d3cd9956959e473ee7131
TehBrian <tehbrian@protonmail.com>
2022-01-05 21:49:34 -0500

    build: only relocate MiniMessage, not adventure

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ tasks {
 
         val libsPackage = "xyz.tehbrian.iteminator.libs"
         relocate("com.google.inject", "$libsPackage.guice")
-        relocate("net.kyori.adventure", "$libsPackage.adventure")
+        relocate("net.kyori.adventure.text.minimessage", "$libsPackage.minimessage")
         relocate("org.spongepowered.configurate", "$libsPackage.configurate")
         relocate("dev.tehbrian.tehlib", "$libsPackage.tehlib")
         relocate("broccolai.corn", "$libsPackage.corn")
