e024ce455528b7557309d33c56d8ebc98078a578
TehBrian <tehbrian@proton.me>
2022-12-08 12:42:31 -0500

    deps: update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
   id("java")
   id("com.github.johnrengelman.shadow") version "7.1.2"
-  id("xyz.jpenilla.run-paper") version "1.0.6"
-  id("net.kyori.indra.checkstyle") version "2.1.1"
+  id("xyz.jpenilla.run-paper") version "2.0.0"
+  id("net.kyori.indra.checkstyle") version "3.0.1"
 }
 
 group = "xyz.tehbrian"
@@ -15,29 +15,23 @@ java {
 
 repositories {
   mavenCentral()
-  maven("https://papermc.io/repo/repository/maven-public/") {
-    name = "papermc"
-  }
-  maven("https://repo.broccol.ai/releases/") {
-    name = "broccolai"
-  }
-  maven("https://repo.thbn.me/releases/") {
-    name = "thbn"
-  }
+  maven("https://papermc.io/repo/repository/maven-public/")
+  maven("https://repo.broccol.ai/releases/")
+  maven("https://repo.thbn.me/releases/")
 }
 
 dependencies {
   compileOnly("io.papermc.paper:paper-api:1.19-R0.1-SNAPSHOT")
 
-  implementation("broccolai.corn:corn-minecraft-paper:3.1.0")
-  implementation("cloud.commandframework:cloud-minecraft-extras:1.7.0")
+  implementation("broccolai.corn:corn-minecraft-paper:3.2.0")
+  implementation("cloud.commandframework:cloud-minecraft-extras:1.7.1")
   implementation("com.google.inject:guice:5.1.0")
   implementation("dev.tehbrian:tehlib-paper:0.3.1")
   implementation("org.spongepowered:configurate-yaml:4.1.2")
 
   testImplementation("io.papermc.paper:paper-api:1.19-R0.1-SNAPSHOT")
   testImplementation("org.junit.jupiter:junit-jupiter")
-  testImplementation(platform("org.junit:junit-bom:5.9.0"))
+  testImplementation(platform("org.junit:junit-bom:5.9.1"))
 }
 
 tasks {
