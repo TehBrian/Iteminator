4807183e0ba1094d34c1680bd12a441b484da529
TehBrian <tehbrian@proton.me>
2022-12-08 14:41:36 -0500

    update white and dark blue colors

--- src/main/java/xyz/tehbrian/iteminator/util/Colors.java
+++ src/main/java/xyz/tehbrian/iteminator/util/Colors.java
@@ -7,10 +7,10 @@ import java.util.Objects;
 public final class Colors {
 
   // yeah, I'm wrapping a non-parameter in Objects.requireNonNull. sue me.
-  public static final TextColor WHITE = Objects.requireNonNull(TextColor.fromCSSHexString("#FFEEFF"));
+  public static final TextColor WHITE = Objects.requireNonNull(TextColor.fromCSSHexString("#EEEEFF"));
   public static final TextColor GRAY = Objects.requireNonNull(TextColor.fromCSSHexString("#333344"));
   public static final TextColor LIGHT_BLUE = Objects.requireNonNull(TextColor.fromCSSHexString("#88EEFF"));
-  public static final TextColor DARK_BLUE = Objects.requireNonNull(TextColor.fromCSSHexString("#44BBDD"));
+  public static final TextColor DARK_BLUE = Objects.requireNonNull(TextColor.fromCSSHexString("#44AABB"));
   public static final TextColor RED = Objects.requireNonNull(TextColor.fromCSSHexString("#DD5577"));
 
   private Colors() {
