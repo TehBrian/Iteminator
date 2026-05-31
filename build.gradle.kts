e87997707da1ce9c81e808002488cf49e578f8a2
TehBrian <tehbrian@proton.me>
2026-05-31 14:07:19 -0400

    mc 26.1.2; update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
   id("java")
-  id("com.gradleup.shadow") version "8.3.0"
-  id("xyz.jpenilla.run-paper") version "2.3.0"
-  id("net.kyori.indra.checkstyle") version "3.1.3"
-  id("com.github.ben-manes.versions") version "0.51.0"
+  id("com.gradleup.shadow") version "9.4.2"
+  id("xyz.jpenilla.run-paper") version "3.0.2"
+  id("net.kyori.indra.checkstyle") version "4.0.0"
+  id("com.github.ben-manes.versions") version "0.54.0"
 }
 
 group = "dev.tehbrian"
@@ -11,29 +11,28 @@ version = "0.4.0"
 description = "A modern item editing plugin."
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(25))
 }
 
 repositories {
   mavenCentral()
   maven("https://repo.papermc.io/repository/maven-public/")
-  maven("https://repo.thbn.me/releases/")
+  maven("https://repo.tehbrian.dev/releases/")
   maven("https://repo.broccol.ai/snapshots/")
 }
 
 dependencies {
-  compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
-
+  compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
   implementation("love.broccolai.corn:corn-minecraft:4.0.0-SNAPSHOT")
   implementation("cloud.commandframework:cloud-minecraft-extras:1.8.4")
   implementation("cloud.commandframework:cloud-paper:1.8.4")
   implementation("com.google.inject:guice:7.0.0")
   implementation("dev.tehbrian:tehlib-paper:0.6.0")
-  implementation("org.spongepowered:configurate-yaml:4.1.2")
+  implementation("org.spongepowered:configurate-yaml:4.2.0")
 
-  testImplementation("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+  testImplementation("io.papermc.paper:paper-api:26.1.2.build.+")
   testImplementation("org.junit.jupiter:junit-jupiter")
-  testImplementation(platform("org.junit:junit-bom:5.11.0"))
+  testImplementation(platform("org.junit:junit-bom:6.1.0"))
 }
 
 tasks {
@@ -44,8 +43,10 @@ tasks {
   processResources {
     filesMatching("plugin.yml") {
       expand(
-        "version" to project.version,
-        "description" to project.description
+        mapOf(
+          "version" to project.version,
+          "description" to project.description
+        )
       )
     }
   }
@@ -65,9 +66,12 @@ tasks {
     }
 
     moveToLibs(
-      "broccolai.corn",
-      "cloud.commandframework",
-      "com.google",
+      "love.broccolai",
+      "com.google.common",
+      "com.google.errorprone",
+      "com.google.inject",
+      "com.google.j2objc",
+      "com.google.thirdparty",
       "dev.tehbrian.tehlib",
       "io.leangen",
       "jakarta.inject",
@@ -81,7 +85,7 @@ tasks {
   }
 
   runServer {
-    minecraftVersion("1.21.1")
+    minecraftVersion("26.1.2")
   }
 
   test {
